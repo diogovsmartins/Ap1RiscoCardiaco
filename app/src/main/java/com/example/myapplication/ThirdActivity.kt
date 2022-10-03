0dd2bc3b1426cf23bf92b3dc3dbfc57784d21707
diogovsmartins <diogodmartins@rede.ulbra.br>
2022-10-03 20:41:17 -0300

    updating project

--- app/src/main/java/com/example/myapplication/ThirdActivity.kt
+++ app/src/main/java/com/example/myapplication/ThirdActivity.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myapplication.dataObjects.AllOptionsFromTheAssesment
 import com.example.myapplication.dataObjects.ResultObjectFromFirstPage
 import com.example.myapplication.dataObjects.ResultObjectFromSecondPage
 import com.example.myapplication.databinding.ActivityThirdBinding
@@ -15,42 +14,11 @@ class ThirdActivity : AppCompatActivity() {
         binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
         calculateAssesmentResults()
-        createArrayFromArrayStrings()
     }
 
 
     private fun calculateAssesmentResults() {
         val firstPageResults=intent.getParcelableExtra<ResultObjectFromFirstPage>("firstPageResults")
         val secondPageResults=intent.getParcelableExtra<ResultObjectFromSecondPage>("secondPageResults").toString()
-        val options=createArrayFromArrayStrings()
-
-    }
-
-    private fun something(optionsList: List<String>){
-        val options=optionsList
-        val optionValue= arrayOf(1,2,3,4,5,6)
-
-    }
-
-
-    private fun createArrayFromArrayStrings(): AllOptionsFromTheAssesment {
-      val ageArray=ArrayList(resources.getStringArray(R.array.age_question).toMutableList())
-        val genderArray=ArrayList(resources.getStringArray(R.array.gender_question).toMutableList())
-        val weightArray=ArrayList(resources.getStringArray(R.array.weight_question).toMutableList())
-        val activityArray=ArrayList(resources.getStringArray(R.array.activity_question).toMutableList())
-        val cigaretteArray=ArrayList(resources.getStringArray(R.array.cigarette_question).toMutableList())
-        val pressureQuestionArray=ArrayList(resources.getStringArray(R.array.pressure_question).toMutableList())
-        val heartDiseaseArray=ArrayList(resources.getStringArray(R.array.heart_disease_question).toMutableList())
-        val cholesterolArray=ArrayList(resources.getStringArray(R.array.cholesterol_question).toMutableList())
-      return AllOptionsFromTheAssesment(
-            ageArray,
-            genderArray,
-            weightArray,
-            activityArray,
-            cigaretteArray,
-            pressureQuestionArray,
-            heartDiseaseArray,
-            cholesterolArray
-        )
     }
 }
